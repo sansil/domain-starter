514bc6ff5bde04c2dbda3ea702737c5def5ca089
sansil <sansil32@gmail.com>
2022-02-23 16:35:06 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,27 +1,131 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'sansildev';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
 
-  return (
+	//Just a state variable we use to store our user's public wallet. Don't forget to import useState at the top.
+	const [currentAccount, setCurrentAccount] = useState('');
+	const [domain, setDomain] = useState('');
+	const [record, setRecord] = useState('');
+	const tld = '.hero';
+	const CONTRACT_ADDRESS = 'YOUR_CONTRACT_ADDRESS_HERE';
+
+	// Implement your connectWallet method here
+	const connectWallet = async () => {
+		try {
+			const { ethereum } = window;
+
+			if (!ethereum) {
+				alert("Get MetaMask -> https://metamask.io/");
+				return;
+			}
+
+			// Fancy method to request access to account.
+			const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+			// Boom! This should print out public address once we authorize Metamask.
+			console.log("Connected", accounts[0]);
+			setCurrentAccount(accounts[0]);
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	const checkIfWalletIsConnected = async () => {
+		const { ethereum } = window;
+
+		if (!ethereum) {
+			console.log('Make sure you have metamask!');
+			return;
+		} else {
+			console.log('We have the ethereum object', ethereum);
+		}
+
+		// Check if we're authorized to access the user's wallet
+		const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+		// Users can have multiple authorized accounts, we grab the first one if its there!
+		if (accounts.length !== 0) {
+			const account = accounts[0];
+			console.log('Found an authorized account:', account);
+			setCurrentAccount(account);
+		} else {
+			console.log('No authorized account found');
+		}
+	};
+
+	// Render Methods
+	const renderNotConnectedContainer = () => (
+		<div className="connect-wallet-container">
+			<img src="https://media.giphy.com/media/Xt6Pws4uotO1y/giphy.gif" alt="Ninja donut gif" />
+			{/* Call the connectWallet function we just wrote when the button is clicked */}
+			<button onClick={connectWallet} className="cta-button connect-wallet-button">
+				Connect Wallet
+			</button>
+		</div>
+	);
+
+	// Form to enter domain name and data
+	const renderInputForm = () => {
+		return (
+			<div className="form-container">
+				<div className="first-row">
+					<input
+						type="text"
+						value={domain}
+						placeholder='domain'
+						onChange={e => setDomain(e.target.value)}
+					/>
+					<p className='tld'> {tld} </p>
+				</div>
+
+				<input
+					type="text"
+					value={record}
+					placeholder='whats ur ninja power'
+					onChange={e => setRecord(e.target.value)}
+				/>
+
+				<div className="button-container">
+					<button className='cta-button mint-button' disabled={null} onClick={null}>
+						Mint
+					</button>
+					<button className='cta-button mint-button' disabled={null} onClick={null}>
+						Set data
+					</button>
+				</div>
+
+			</div>
+		);
+	}
+
+	// This runs our function when the page loads.
+	useEffect(() => {
+		checkIfWalletIsConnected();
+	}, [])
+
+	return (
 		<div className="App">
 			<div className="container">
-
 				<div className="header-container">
 					<header>
-            <div className="left">
-              <p className="title">🐱‍👤 Ninja Name Service</p>
-              <p className="subtitle">Your immortal API on the blockchain!</p>
-            </div>
+						<div className="left">
+							<p className="title">🐱‍👤 Hero Name Service</p>
+							<p className="subtitle">Your immortal API on the blockchain!</p>
+						</div>
 					</header>
 				</div>
 
-        <div className="footer-container">
+				{!currentAccount && renderNotConnectedContainer()}
+				{/* Render the input form if an account is connected */}
+				{currentAccount && renderInputForm()}
+
+				<div className="footer-container">
 					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
 					<a
 						className="footer-text"
@@ -33,6 +137,6 @@ const App = () => {
 			</div>
 		</div>
 	);
-}
+};
 
 export default App;
